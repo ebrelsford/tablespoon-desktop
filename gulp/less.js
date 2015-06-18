c5af5049805a7396d404cb9c0064d995a3c9bba7
Eric Brelsford <ebrelsford@gmail.com>
2015-06-18 16:36:00 -0400

    Gulp LESS files

--- gulp/less.js
+++ gulp/less.js
@@ -4,5 +4,5 @@ var less = require('gulp-less');
 gulp.task('less', function () {
 	return gulp.src('./style/**/*.less')
 		.pipe(less())
-		.pipe(gulp.dest('./compile/style'));
+		.pipe(gulp.dest('./style'));
 });
--- gulp/through.js
+++ gulp/through.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 
 gulp.task('through', function () {
-	return gulp.src(['index.html', 'templates/**/*', '*.js'], { base: '.' })
+	return gulp.src([
+        'index.html',
+        'style/base.css',
+        'templates/**/*',
+        '*.js'
+    ], { base: '.' })
 		.pipe(gulp.dest('./compile'));
 });
